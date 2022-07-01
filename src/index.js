1d46db0abc5f4efd56369bce38474cd00013c518
SvitlanaVelychko <svel2608@gmail.com>
2022-07-01 23:19:26 +0300

    Solve task

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import { createImageCards } from "./js/createImageCards";
 const refs = {
     form: document.querySelector('.search-form'),
     imagesContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more'),
 }
 let imageName = '';
 let counterImages = 0;
 
 refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 let lightbox = new SimpleLightbox('.photo-card a', {
     captions: true,
@@ -24,19 +26,21 @@ async function onFormSubmit(e) {
     e.preventDefault();
     clearGalleryContainer();
 
-    imageName = refs.form.elements.searchQuery.value;
+    imageName = refs.form.elements.searchQuery.value.trim();
 
     try {
-        resetPage();
+    resetPage();
+    refs.loadMoreBtn.classList.remove('is-visible');
         await fetchImages(imageName).then(({ images, totalHits }) => {
-            refs.imagesContainer.innerHTML = createImageCards(images);
-            lightbox.refresh();
-            counterImages = images.length;
-
             if (images.length === 0 || imageName === '') {
                 clearGalleryContainer();
+                refs.loadMoreBtn.classList.remove('is-visible');
                 Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
             } else {
+                refs.imagesContainer.innerHTML = createImageCards(images);
+                lightbox.refresh();
+                refs.loadMoreBtn.classList.add('is-visible');
+                counterImages = images.length;
                 Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
             }
         });
@@ -45,36 +49,24 @@ async function onFormSubmit(e) {
     }
 }
 
-async function onLoadMoreImages() {
+async function onLoadMoreBtnClick() {
     try {
-        const options = {
-            rootMargin: '0px',
-            threshold: 1.0,
-        };
+        await fetchImages(imageName).then(({ images, totalHits }) => {
+            refs.imagesContainer.insertAdjacentHTML('beforeend', createImageCards(images));
+            lightbox.refresh();
+            counterImages += images.length;
 
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    fetchImages(imageName).then(({ images, totalHits }) => {
-                        refs.imagesContainer.insertAdjacentHTML('beforeend', createImageCards(images));
-                        lightbox.refresh();
-                        counterImages += images.length;
-
-                        if (counterImages > totalHits) {
-                            setTimeout(() => {
-                                Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-                            }, 2000);
-                        }
-                    });
-                }
-            })
-        }, options);
-
-        observer.observe(document.querySelector('.scroll-guard'));
+            if (counterImages > totalHits) {
+                refs.loadMoreBtn.classList.remove('is-visible');
+                setTimeout(() => {
+                    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+                }, 2000);
+            }
+        });
     } catch (error) {
         console.log(error);
     }
-} 
+}
 
 function clearGalleryContainer() {
     refs.imagesContainer.innerHTML = '';
